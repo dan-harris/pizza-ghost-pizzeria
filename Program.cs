2f9a456866cff14d2b5a0804cb55c4263415cd93
Dan Harris <Dan.Harris@hfoh.com.au>
2018-11-01 09:11:28 +1030

    feat(initial-page-setup): did some initial page setup with partials & scripts + inline styles

--- Program.cs
+++ Program.cs
@@ -7,18 +7,46 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 
-namespace PizzaGhostPizzeria
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
+namespace PizzaGhostPizzeria {
+    public class Program {
+        /// <summary>
+        /// constants for env var settings
+        /// </summary>
+        public const string ASPNETCORE_ENVIRONMENT_VAR = "ASPNETCORE_ENVIRONMENT";
+        public const string ENV_SETTINGS_FILENAME = "envsettings.json";
+
+        /// <summary>
+        /// set the default environment variable
+        /// </summary>
+        private static string _environment = "dev";
+
+        /// <summary> 
+        /// start web server
+        /// </summary>
+        public static void Main (string[] args) {
+            CreateWebHostBuilder (args).Build ().Run ();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        /// <summary>
+        /// web host builder
+        /// </summary>
+        public static IWebHostBuilder CreateWebHostBuilder (string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseEnvironment (_environment)
+            .UseStartup<Startup> ();
+
+        /// <summary>
+        /// load the environment value from json file
+        /// </summary>
+        public static void SetEnvironment () {
+            var currentDirectoryPath = Directory.GetCurrentDirectory ();
+            var envSettingsPath = Path.Combine (currentDirectoryPath, ENV_SETTINGS_FILENAME);
+            if (File.Exists (envSettingsPath)) {
+                var envSettings = JObject.Parse (File.ReadAllText (envSettingsPath));
+                _environment = envSettings[ASPNETCORE_ENVIRONMENT_VAR].ToString ();
+            }
+        }
     }
 }
---
+++ src/TagHelpers/ClientStyleSheetTagHelper.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace PizzaGhostPizzeria.TagHelpers {
+
+    [HtmlTargetElement ("link", Attributes = "client-style")]
+    public class ClientStyleSheetTagHelper : TagHelper {
+
+        public string ClientStyle { get; set; }
+
+        private readonly string _clientStylesheetPath = "client-dist";
+
+        /// <summary>
+        /// set the href attribute with the client-app style sheet location
+        /// </summary>
+        public override void Process (TagHelperContext context, TagHelperOutput output) {
+            output.Attributes.SetAttribute ("rel", "stylesheet");
+            output.Attributes.SetAttribute ("href", $"{this._clientStylesheetPath}/{this.ClientStyle}.css");
+        }
+
+    }
+}
